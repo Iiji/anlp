8be5c884ed0cff85b0dfb89e367290313aac37c9
Iiji <jli2short@gmail.com>
2024-12-09 23:04:21 -0600

    upd

--- wordle/build.sh
+++ wordle/build.sh
@@ -1,7 +1,7 @@
-# python convert-train.py \
-#     --data_path ../data/trajectories/wordle/train \
-#     --response_template v0 \
-#     --one_img
+python convert-train.py \
+    --data_path ../data/trajectories/wordle/train \
+    --response_template v0 \
+    --one_img
 
 python convert-test.py \
     --data_path ../data/trajectories/wordle/test \
--- wordle/eval_llava.sh
+++ wordle/eval_llava.sh
@@ -33,3 +33,37 @@ for IDX in $(seq 0 $((CHUNKS-1))); do
 done
 
 python wordle/eval_match.py --result_path ./data/eval/wordle/answers/$SPLIT/$CKPT/
+
+
+
+
+
+
+CKPT="llava-v1.5-7b"
+SPLIT="skill_test"
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python wordle/inference.py \
+        --model-path liuhaotian/llava-v1.5-7b \
+        --question-file ./data/trajectories/wordle/test/test_skills_v0.json \
+        --image-folder ./data/trajectories/wordle/test/ \
+        --answers-file ./data/eval/wordle/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX \
+        --temperature 0 \
+        --conv-mode vicuna_v1 &
+done
+
+wait
+
+output_file=./data/eval/wordle/answers/$SPLIT/$CKPT/merge.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat ./data/eval/wordle/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
+
+python wordle/eval_skills.py --result_path ./data/eval/wordle/answers/$SPLIT/$CKPT/ --skip_first_step
